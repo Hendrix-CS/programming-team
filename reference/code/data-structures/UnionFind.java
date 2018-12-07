9f0f79f35a7206ae1ee2df5f4676d9eff930438f
Brent Yorgey <byorgey@gmail.com>
2018-12-06 22:12:37 -0600

    update union-find

--- reference/code/data-structures/UnionFind.java
+++ reference/code/data-structures/UnionFind.java
@@ -1,29 +1,28 @@
-public class UnionFind {
-    private byte[] r; private int[] p;
+class UnionFind {
+    private byte[] r; private int[] p;  // rank, parent
+
+    // Make a new union-find structure with n items in singleton sets,
+    // numbered 0 .. n-1 .
     public UnionFind(int n) {
         r = new byte[n]; p = new int[n];
         for (int i = 0; i < n; i++) {
             r[i] = 0; p[i] = i;
         }
     }
+
+    // Return the root of the set containing v, with path compression. O(1).
+    // Test whether u and v are in the same set with find(u) == find(v).
     public int find(int v) {
-        while (v != p[v]) {
-            p[v] = p[p[v]]; v = p[v];
-        }
-        return v;
+        return v == p[v] ? v : (p[v] = find(p[v]));
     }
-    public boolean connected(int u, int v) {
-        return find(u) == find(v);
-    }
+
+    // Union the sets containing u and v. O(1).
     public void union(int u, int v) {
         int ru = find(u), rv = find(v);
         if (ru != rv) {
             if      (r[ru] > r[rv]) p[rv] = ru;
             else if (r[rv] > r[ru]) p[ru] = rv;
-            else {
-                p[ru] = rv;
-                r[rv]++;
-            }
+            else { p[ru] = rv; r[rv]++; }
         }
     }
 }
