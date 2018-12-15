9f826909b4baf16af7cf1ca063dcf12e027d7ee9
Brent Yorgey <byorgey@gmail.com>
2018-12-15 17:10:16 -0600

    update MD5 annotation program

--- reference/check.sh
+++
@@ -1,9 +0,0 @@
-function check {
-    IFS=
-    s=""
-    cat $1 | while read l; do
-        s="$s$(echo $l | sed 's/\s//g')\n"
-        h=$(echo -ne "$s" | md5sum)
-        echo "${h:0:2}  $l"
-    done
-}
--- reference/code/java/AnnotateMD5.java
+++ reference/code/java/AnnotateMD5.java
@@ -3,28 +3,31 @@ import java.util.*;
 import java.math.*;
 import java.security.*;
 
-public class CheckMD5 {
-
-    public static String md5Sig(String input) {
+public class AnnotateMD5 {
+    public static String md5Sig(String s) {
         try {
-            byte[] bs = MessageDigest.getInstance("MD5").digest(input.getBytes());
+            byte[] bs = MessageDigest.getInstance("MD5").digest(s.getBytes());
             return String.format("%02x", bs[0]);
         } catch (NoSuchAlgorithmException e) {
             return "";
         }
     }
-
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("File to annotate: ");
-        String file = in.nextLine();
-
+        String file = "";
+        if (args.length > 0) file = args[0];
+        else {
+            Scanner in = new Scanner(System.in);
+            System.out.print("File to annotate: ");
+            file = in.nextLine();
+        }
         try {
             Scanner f = new Scanner(new File(file));
             while (f.hasNext()) {
                 String line = f.nextLine();
                 String sig = md5Sig(line.replaceAll("\\s", ""));
-                System.out.println((line.equals("") ? "  " : sig) + " " + line);
+                System.out.print(line);
+                for (int i = line.length(); i < 80; i++) System.out.print(" ");
+                System.out.println(" // " + sig);
             }
         } catch (FileNotFoundException e) { System.out.println(e); }
     }
