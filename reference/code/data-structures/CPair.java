ce7dcffd99546c6380427b72c8f0ff1f7ed59739
Brent Yorgey <byorgey@gmail.com>
2019-09-18 19:58:01 -0500

    stuff!

--- reference/code/data-structures/CPair.java
+++ reference/code/data-structures/CPair.java
@@ -1,6 +1,7 @@
 import java.util.Comparator;
 
-public class CPair<A extends Comparable<A>, B extends Comparable<B>> implements Comparable<CPair<A,B>> {
+public class CPair<A extends Comparable<A>, B extends Comparable<B>>
+      implements Comparable<CPair<A,B>> {
     A a; B b;
     public CPair(A a, B b) { this.a = a; this.b = b; }
     public int hashCode() { return 31 * (31 * 7 + a.hashCode()) + b.hashCode(); }
@@ -9,7 +10,9 @@ public class CPair<A extends Comparable<A>, B extends Comparable<B>> implements 
         return a.equals(p.a) && b.equals(p.b);
     }
     public String toString() { return "(" + a + "," + b + ")"; }
-    public int compareTo(CPair<A,B> p) {
-        return Comparator.comparing(q -> q.a).thenComparing(q -> q.b).compare(p);
+    public int compareTo(CPair<A,B> p) {   // sort by a, then b
+        if (!a.equals(p.a)) return a.compareTo(p.a);
+        else                return b.compareTo(p.b);
     }
+
 }
--- reference/code/data-structures/PairTest.java
+++ reference/code/data-structures/PairTest.java
@@ -12,5 +12,16 @@ public class PairTest {
         System.out.println(s.size() == 2 ? "OK" : "o noes");
         System.out.println(s.contains(p) ? "OK" : "o noes");
         System.out.println(!s.contains(new Pair<Integer, String>(65, "box")) ? "OK" : "o noes");
+
+        TreeSet<CPair<Integer, String>> t = new TreeSet<>();
+        t.add(new CPair<Integer, String>(20, "fox"));
+        t.add(new CPair<Integer, String>(20, "box"));
+        t.add(new CPair<Integer, String>(19, "fox"));
+        t.add(new CPair<Integer, String>(33, "abc"));
+        t.add(new CPair<Integer, String>(20, "fog"));
+
+        for (CPair<Integer, String> pair : t) {
+            System.out.println(pair);
+        }
     }
 }
