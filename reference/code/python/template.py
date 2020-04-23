2d2bc389b09b11d1007b25dbf97d9ffe4b178a90
Brent Yorgey <byorgey@gmail.com>
2020-04-23 08:46:23 -0500

    simplify Python reference code

--- reference/code/python/template.py
+++ reference/code/python/template.py
@@ -1,12 +1,9 @@
-import sys
 
-if __name__ == '__main__':
+n = int(input())  # Read an int on a line by itself
+for _ in range(n):             # Do something n times
 
-    n = int(sys.stdin.readline())  # Read an int on a line by itself
-    for _ in range(n):             # Do something n times
+    # Read all the ints on a line into a list
+    xs = map(int, input().split())
 
-        # Read all the ints on a line into a list
-        xs = map(int, sys.stdin.readline().split())
-
-        # Read a known number of ints into variables
-        p, q, r, y = map(int, sys.stdin.readline().split())
+    # Read a known number of ints into variables
+    p, q, r, y = map(int, input().split())
